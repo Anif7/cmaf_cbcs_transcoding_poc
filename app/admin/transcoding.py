e53eb9286294b24636f3d36c1631d3a346256cde
Anif7 <anif@testpress.in>
2026-03-26 23:03:07 +0530

    fix: Add Task retry action in admin page

--- app/admin/transcoding.py
+++ app/admin/transcoding.py
@@ -1,9 +1,17 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from ..models import TranscodingJob
+from ..tasks import video_transcoding_task
 
 @admin.register(TranscodingJob)
 class TranscodingJobAdmin(admin.ModelAdmin):
-    list_display = ('id', 'input_url', 'status', 'created', 'modified')
+    list_display = ('id', 'status', 'created', 'modified')
     list_filter = ('status', 'created')
-    search_fields = ('input_url', 'id')
+    search_fields = ('id',)
     readonly_fields = ('created', 'modified')
+    actions = ['retry_jobs']
+
+    @admin.action(description="Retry selected transcoding jobs")
+    def retry_jobs(self, request, queryset):
+        for job in queryset:
+            video_transcoding_task.delay(job.id)
+        self.message_user(request, f"Retried {queryset.count()} jobs.", messages.SUCCESS)
